00a7a0fb991fabd51979bd79b28c1a96d18365e5
João Coimbra <joao.benatti.coimbra@gmail.com>
2022-12-08 13:09:29 -0300

    add fonts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/global.css';
-// import './styles/fonts.css';
+import './styles/fonts.css';
 import PageRouter from './pages/Router';
 import reportWebVitals from './reportWebVitals';
 

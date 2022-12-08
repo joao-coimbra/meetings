0c904ec50ee55969187781acd5b41c4bf402cac6
João Coimbra <joao.benatti.coimbra@gmail.com>
2022-12-08 13:09:57 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				"base-color": "#1972b9",
+				"secondary-color": "#50a7ee",
+				"secondary-color-hover": "#6bb8f7",
+				"terciary-color": "#ff5722",
+			},
+		},
+	},
+	plugins: [],
+};

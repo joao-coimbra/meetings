f0080c1796cf0a8b1eeb19bcb798b65b2a8bbca2
João Coimbra <joao.benatti.coimbra@gmail.com>
2022-12-08 13:09:47 -0300

    Update auth.api.js

--- src/services/api/auth.api.js
+++ src/services/api/auth.api.js
@@ -1,15 +1,21 @@
 import axios from "axios";
+import { LocalStorage } from "../cache/LocalStorage.service";
 
 const API_URL = process.env.REACT_APP_BASE_URL_API + "/auth";
 
 export const api = {
-	signin(id, user) {
-		return axios.post(API_URL + "/login-google", { id, user });
+	signin(username, password) {
+		username = username+'@linearequipamentos.com.br'
+		return axios.post(API_URL + "/m-login", { username, password });
 	},
 	logout() {
 		// LocalStorage.remove("token");
 	},
 	verify() {
-		return axios.get(`${API_URL}/verify-google`);
+		return axios.get(`${API_URL}/m-verify`, {
+			headers: {
+				token: LocalStorage.get("m-token"),
+			},
+		});
 	},
 };
